5938d099ed09302c8e3adb6cdbb0c73e02000662
abdulloh19 <parij4320@gmail.com>
2022-05-04 11:28:42 +0500

    fucntions

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const stringE = function(str) {
+    let ecount = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+
+        if(str[i] == "e") {
+            ecount++
+        }
+    }
+    if (ecount <= 3) {
+
+        return true
+    }
+    return false
+}
+const hase = stringE("heeello")
+console.log(hase);
+
+
+const parrotTrouble = function(talking, hour) {
+    
+    if (talking && (hour <= 7 || talking && hour >= 20)) {
+        return true
+    }
+        return false
+}
+console.log(parrotTrouble(true || false, 19));
+
+// https://github.com/abdulloh19/for-function.git
